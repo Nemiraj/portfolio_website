505bab219dcd608f53bac7139b42cb7e8f79b3fa
Nemiraj <59046951+Nemiraj@users.noreply.github.com>
2024-07-05 22:38:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/portfolio_website/',  // Ensure this matches your repository name
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory here
+    outDir: 'dist',
   },
 });
